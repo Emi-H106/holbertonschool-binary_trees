1c03b42ad863bd246c9e81250fbf41566adcaa0f
lnqbat <b.lonqueu98@gmail.com>
2025-04-07 10:15:19 +0200

    add header file new prototype and add library std

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,11 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,6 +23,14 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/* Mandatory */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 
 #endif
