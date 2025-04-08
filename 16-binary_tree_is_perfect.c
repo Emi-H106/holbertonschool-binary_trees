8561804a520c49dc5f356b076d56a8d7ea710631
Emi-H106 <emi.hatano@yahoo.com>
2025-04-08 17:47:48 +0200

    fix betty style

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -57,7 +57,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (left_height != right_height)
 		return (0);
 
-	
+
 	return (binary_tree_is_perfect(tree->left) &&
 		binary_tree_is_perfect(tree->right));
 }
